a0be2133a67d3561f3537c32d9b4409f2cb214f4
Itish-Raj-Singh <94170586+Itish-Raj-Singh@users.noreply.github.com>
2022-04-10 16:04:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+leftWrist_x = 0;
+rightWrist_x = 0;
+difference = 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(400,400);
@@ -6,21 +10,33 @@ function setup(){
     canvas = createCanvas(800,400);
     canvas.position(430,130);
 
-    poseNet = ml5.poseNet(video,modelloaded);
+    poseNet = ml5.poseNet(video,modelDone);
     poseNet.on('pose',gotposes);
 }
 
 function draw(){
     background("#5196e3");
+    document.getElementById("size_font").innerHTML = "Font Size Of The Text Will Be = "+difference+"px";
+    textSize(difference);
+    fill("#00ff0a");
+    text('Itish',50,400);
 }
 
-function modelloaded(){
+function modelDone(){
     console.log("PoseNet Is Initialized And Loaded");
 }
 
-function gotposes(results){
-       if(results.length > 0){
+function gotposes(results,error){
+    if(error){
+        console.error(error);
+    }
+    if(results.length > 0){
         console.log(results);
+
+        leftWrist_x = results[0].pose.leftWrist.x;
+        rightWrist_x = results[0].pose.rightWrist.x;
+
+        difference = floor(leftWrist_x - rightWrist_x);
 
         console.log("rightWrist_x = "+results[0].pose.rightWrist.x + " rightWrist_y = "+results[0].pose.rightWrist.y);
         console.log("leftWrist_x = "+results[0].pose.leftWrist.x + " leftWrist_y = "+results[0].pose.leftWrist.y);
