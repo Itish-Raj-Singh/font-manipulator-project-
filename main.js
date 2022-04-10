11804fb04649e64f3cd42d93407cee3833fea570
Itish-Raj-Singh <94170586+Itish-Raj-Singh@users.noreply.github.com>
2022-04-10 15:49:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
 function setup(){
     video = createCapture(VIDEO);
-    canvas = createCanvas(550 , 550);
-    canvas.center();
+    video.size(400,400);
+    video.position(10,50);
 
-   poseNet= ml5.poseNet(VIDEO , modelloaded);
-   poseNet.on('pose' , gotPoses);
+    canvas = createCanvas(800,400);
+    canvas.position(430,130);
+
+    poseNet = ml5.poseNet(video,modelloaded);
+    poseNet.on('pose',gotposes);
 }
-function modelloaded(){
-    console.log('PoseNet Is Initialiazed');
-}
-function gotPoses(results){
-    if(results.length > 0){
-        console.log(results);
-    }
-}
+
 function draw(){
-    image(video , 15 ,15 , 550 ,550);
+    background("#5196e3");
+}
+
+function modelloaded(){
+    console.log("PoseNet Is Initialized And Loaded");
+}
+
+function gotposes(results){
+       if(results.length > 0){
+        console.log(results);
+
+        console.log("rightWrist_x = "+results[0].pose.rightWrist.x + " rightWrist_y = "+results[0].pose.rightWrist.y);
+        console.log("leftWrist_x = "+results[0].pose.leftWrist.x + " leftWrist_y = "+results[0].pose.leftWrist.y);
+    }
 }
